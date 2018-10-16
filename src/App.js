b785085f67e6fe2803812628ccb9ee65f831568b
Pedro Henrique <pedrohenrique@pedros-mbp.localdomain>
2018-10-16 05:43:59 -0300

    Fix imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-//import firebase from 'firebase';
 import firebase from '@firebase/app'
 import ReduxThunk from 'redux-thunk';
 
